c57b1e78fb6c40c5acbc07886f0f6da474cf807e
mmyleung <minamleung@gmail.com>
2023-10-20 07:47:00 +0100

    update config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2 (eu-west-2 is London)
+        userPoolId: 'eu-north-1_LTC3PUSSt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2nb6csj73dp8m7e2ula9k3g0ae', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2 (eu-west-2 is London)
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
